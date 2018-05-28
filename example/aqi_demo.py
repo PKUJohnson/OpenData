d22ea3989dfa4b46cdf19ef61d111e6241da8853
PKUJohnson <14203244@qq.com>
2018-05-28 10:59:05 +0800

    init project

---
+++ example/aqi_demo.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+from opendatatools import aqi
+
+
+# 获取某日全国各大城市的AQI数据
+#aqi.get_daily_aqi('2018-01-01')
+
+# 获取单个城市的AQI历史数据
+#aqi.get_daily_aqi_onecity('北京市')
+
+#获取单个城市某日的AQI小时数据
+aqi_hour = aqi.get_hour_aqi_onecity('北京市', '2018-05-26')
+aqi_hour.set_index('time', inplace=True)
+aqi_hour
