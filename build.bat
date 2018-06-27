64c5ddd509c0a8d860df12d8e9fe5c28f189185d
PKUJohnson <14203244@qq.com>
2018-06-27 10:50:15 +0800

    only support python3

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
+del /f/s/q dist
+del /f/s/q build
+del /f/s/q OpenDataTools.egg-info
+git pull
 python setup.py sdist
-python setup.py bdist_wheel --universal
+python setup.py bdist_wheel
 twine upload dist/*
 pause
