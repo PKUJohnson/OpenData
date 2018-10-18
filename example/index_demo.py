18ad92182bd5d6abc66bfcd1d8fdc388e4ee03ce
PKUJohnson <14203244@qq.com>
2018-10-18 10:46:41 +0800

    fix bug

--- example/index_demo.py
+++ example/index_demo.py
@@ -1,13 +1,16 @@
+
+# encoding: utf-8
+
 from opendatatools import index
 
 if __name__ == '__main__':
     index.set_proxies({"https" : "https://127.0.0.1:1080"})
 
-    # å…¨çƒè‚¡ç¥¨æŒ‡æ•°åˆ—è¡¨
+    # È«Çò¹ÉÆ±Ö¸ÊıÁĞ±í
     df, msg = index.get_index_list()
     print(df)
 
-    # è·å–æŒ‡æ•°æ•°æ®
-    # æ—¶é—´ï¼š1hã€1dã€1wã€1mã€3mã€6mã€1yã€5yã€max
+    # »ñÈ¡Ö¸ÊıÊı¾İ
+    # Ê±¼ä£º1h¡¢1d¡¢1w¡¢1m¡¢3m¡¢6m¡¢1y¡¢5y¡¢max
     df, msd = index.get_index_data(symbol='SSEC', freq='1d', period='max')
     print(df)
